c00e2ea307fac092832d14546b3faa436455df80
Nguyen Huu Tuan <tuannhhe172829@fpt.edu.vn>
2025-05-27 21:28:12 +0700

    finished static gateway

--- gateway-service/src/main/java/project/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/project/gateway/GatewayApplication.java
@@ -2,10 +2,8 @@ package project.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
 public class GatewayApplication 
 {
     public static void main( String[] args )
